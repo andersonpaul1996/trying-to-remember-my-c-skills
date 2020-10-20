8b4175fcb6309807a713082fc2bd647e6a231e1b
kirill050 <kirilltikhonov050@gmail.com>
2020-10-20 22:46:01 +0300

    Saving at GIT

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <any>
+#include <map>
+using namespace std;
+
+class Json {
+public:
+    //  онструктор из строки, содержащей Json-данные.
+    Json(const std::string& s){
+        mapa[string("hello")] = s;
+    }
+    std::any& operator[](const std::string& key){
+        if (mapa[key].type() == typeid(string))
+    {
+        return mapa[key];
+        //any_cast<const std::string&>(_parsed_json[key]);
+    }
+    }
+
+
+public:
+    std::map <std::string, std::any> mapa;
+};
+
+int main()
+{
+    Json JSON(string("Dolly"));
+    cout << std::any_cast<string>(JSON[string("hello")]);
+    //EXPECT_EQ(std::any_cast<std::string>(object["lastname"]), "Ivanov");
+    return 0;
+}
